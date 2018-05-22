fa000632b16a0a9464849704de48a02526a9adb4
Mark Channer <markchanner@gmail.com>
2018-05-22 11:45:03 +0100

    Adds /listings get and post routes, and creates Spaces class that handles adding and removing elements from its array

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'sinatra/base'
+require './lib/spaces.rb'
 
 class MakersBnB < Sinatra::Base
 
-  get '/' do
-    'Hello world!'
+  get '/listings' do
+    @spaces = Spaces.all
+    erb(:index)
   end
 
+  post '/listings' do
+    flatName = params['title']
+    Spaces.add(flatName)
+    redirect '/'
+  end
 
 run! if app_file == $0
 
