31d3c218ea1ec964886dea7f3e96970827e70c68
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-05-22 11:07:48 +0100

    Add form & tests for Homepagecontent & failing test for new listing

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,21 @@
+
+feature 'Homepage' do
+
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Bork BnB'
+  end
+
+  scenario 'Shows form to list your Bork' do
+    visit ('/')
+    expect(page).to have_content 'List your pet space here:'
+  end
+
+  scenario 'Can submit form & listing shows on page' do
+    visit ('/')
+    fill_in 'title', with: "5* Putney Pug Pit"
+    click_button 'List your Bork'
+    expect(page).to have_content '5* Putney Pug Pit'
+  end
+
+end
