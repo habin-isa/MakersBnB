43ad850043ed1ce52a295c193273b9f9306de0e3
Habin <habin@hotmail.co.uk>
2018-05-23 14:17:26 +0100

    add test for description input in form

---
+++ spec/features/spaces_view_spec.rb
@@ -0,0 +1,21 @@
+feature 'Spaces' do
+
+  scenario 'Can run app and check page content' do
+    visit('/spaces')
+    expect(page).to have_content 'Bork BnB'
+  end
+
+  scenario 'Shows form to list your Bork' do
+    visit ('/spaces')
+    expect(page).to have_content 'List your Bork!'
+  end
+
+  scenario 'Can submit title and description, shows on page' do
+    visit ('/spaces')
+    fill_in 'title', with: "5* Putney Pug Pit"
+    fill_in 'description', with: "A quiet and quaint getaway in South London"
+    click_button 'List your Bork'
+    expect(page).to have_content '5* Putney Pug Pit'
+  end
+
+end
