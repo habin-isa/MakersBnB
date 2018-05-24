8cc46d01f2cf602146cbaabb65ddfa8873d8e62a
Habin <habin@hotmail.co.uk>
2018-05-24 14:43:00 +0100

    only display title and description on page, test passes

--- spec/features/spaces_view_spec.rb
+++ spec/features/spaces_view_spec.rb
@@ -10,19 +10,21 @@ feature 'Spaces' do
     expect(page).to have_content 'List your Bork!'
   end
 
-  scenario 'Can submit title and description, shows on page' do
-    visit ('/spaces/new')
-    fill_in 'title', with: "5* Putney Pug Pit"
-    fill_in 'description', with: "A quiet and quaint getaway in South London"
-    fill_in 'price', with: 40
-    click_button 'List your Bork'
-    expect(page).to have_content '5* Putney Pug Pit'
-  end
-
   scenario 'Can click List your Bork! to takes you to spaces/new' do
     visit ('/spaces')
     click_on 'List your Bork!'
     expect(page).to have_current_path('/spaces/new')
+  end
+
+  scenario 'Submit listing and only title and description to appear on page' do
+    visit ('/spaces/new')
+    fill_in 'title', with: "5* Hotel"
+    fill_in 'description', with: "A lovely quiet place"
+    fill_in 'price', with: 91234
+    click_button 'List your Bork'
+    expect(page).to have_content "5* Hotel"
+    expect(page).to have_content "A lovely quiet place"
+    expect(page).not_to have_content 91234
   end
 
 end
